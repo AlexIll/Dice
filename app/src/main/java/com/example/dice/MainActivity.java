cc978a0d6024424f79354d89c7c9bb447605cb9b
AlexIll <alexanderilyov00@gmail.com>
2021-03-05 09:35:45 +0200

    added textview

--- app/src/main/java/com/example/dice/MainActivity.java
+++ app/src/main/java/com/example/dice/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.dice;
 
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 public class MainActivity extends AppCompatActivity {
+
     private ImageView imageViewDice;
     private Random rng = new Random();
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,24 +30,31 @@ public class MainActivity extends AppCompatActivity {
     }
     private void rollDice() {
         int randomNumber = rng.nextInt(6) + 1;
+        TextView textView = (TextView) findViewById(R.id.diceText);
         switch (randomNumber) {
             case 1:
                 imageViewDice.setImageResource(R.drawable.dice1);
+                textView.setText("You Rolled One");
                 break;
             case 2:
                 imageViewDice.setImageResource(R.drawable.dice2);
+                textView.setText("You Rolled Two");
                 break;
             case 3:
                 imageViewDice.setImageResource(R.drawable.dice3);
+                textView.setText("You Rolled Three");
                 break;
             case 4:
                 imageViewDice.setImageResource(R.drawable.dice4);
+                textView.setText("You Rolled Four");
                 break;
             case 5:
                 imageViewDice.setImageResource(R.drawable.dice5);
+                textView.setText("You Rolled Five");
                 break;
             case 6:
                 imageViewDice.setImageResource(R.drawable.dice6);
+                textView.setText("You Rolled Six");
                 break;
         }
     }
